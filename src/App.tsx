dddd87b89efdd13f1b7eb30cbc4e948493f7e910
mahdi-mallaee <mahdi.mallaee@gmail.com>
2025-07-11 09:55:36 +0330

    Add background image;

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import Header from "./components/header"
 
 function App() {
   return (
-    <div className='bg-background text-on-background'>
-      <Header />
+    <div className='relative bg-background text-on-background min-h-screen'>
+      <div
+        className='absolute h-full bg-[url("./assets/bg-tile.png")] bg-repeat inset-0 opacity-10 pointer-events-none ' />
+      <div className='relative'>
+        <Header />
+      </div>
     </div>
   )
 }
